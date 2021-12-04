68782ca339ca0d1e4bdf5cda895dc1ea797b1864
jesuslautaro <jesus.lautaro99@gmail.com>
2021-12-03 22:36:49 -0300

    aaaaaa

--- segundoParcial/app-registrodeventas/database/migrations/2021_08_30_223611_create_registroventas_table.php
+++ segundoParcial/app-registrodeventas/database/migrations/2021_08_30_223611_create_registroventas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVentasTable extends Migration
+class CreateVentasTables extends Migration
 {
     /**
      * Run the migrations.
