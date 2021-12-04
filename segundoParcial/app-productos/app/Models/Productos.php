12715603f7ed1c0620b9e0ab2a7ae1e4ecdd7aa3
jesuslautaro <jesus.lautaro99@gmail.com>
2021-12-03 21:56:20 -0300

    ajustes por todos lados

--- segundoParcial/app-productos/app/Models/Productos.php
+++ segundoParcial/app-productos/app/Models/Productos.php
@@ -1,16 +1,17 @@
 <?php
 
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Laravel\Passport\HasApiTokens;
 
 class Productos extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    use HasApiTokens;
 }
+
+
+
+
