a1689ad154049cf08f215117dd3952af3b9652aa
jesuslautaro <jesus.lautaro99@gmail.com>
2021-12-07 03:44:30 -0300

    controlador producto, a mimir

--- segundoParcial/app-frontend/app/Http/Controllers/ProductoController.php
+++ segundoParcial/app-frontend/app/Http/Controllers/ProductoController.php
@@ -2,12 +2,90 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
-class Controller extends BaseController
+
+class ProductoController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function ListarProductos(Request $request){
+        $producto = Http::get(getenv("APP_PRODUCTOS_URL") . "productos") -> json();
+        return view('listarProductos',["productos" => $producto]);
+    }
+
+    public function AgregarProducto(Request $request){
+        $response = Http::post(getenv("APP_PRODUCTOS_URL") . "productos", [
+            'nombre' => $request -> post('nombre'),
+            'descripcion' => $request -> post('descripcion'),
+            'stock' => $request -> post('stock'),
+        ]) -> json();
+        if($response["resultado"] === "OK")
+        return view('formAgregarProducto',["exito" => true]);
+        else {
+            return "Error";
+        }
+
+    }
+
+    public function ModificarProducto(Request $request){
+        $response = Http::put(getenv("APP_PRODUCTOS_URL") . "productos", [
+            'id' => $request -> post('id'),
+            'nombre' => $request -> post('nombre'),
+            'descripcion' => $request -> post('descripcion'),
+            'stock' => $request -> post('stock'),
+        ]) -> json();
+        if($response["resultado"] === "OK")
+        return view('formModificarProducto',["exito" => true]);
+        else {
+            return "Error";
+        }
+
+    }
+
+    public function EliminarProducto(Request $request){
+        $response = Http::delete(getenv("APP_PRODUCTOS_URL") . "productos", [
+            'id' => $request -> post('id'),
+        ]) -> json();
+
+        if($response["resultado"]=== "OK")
+        return view('formEliminarProducto',["exito" => true]);
+        }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
